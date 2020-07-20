d1d6a1b32f73f790e2599d698fcb855e560bc96a
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-20 14:10:44 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   # loop and find the maximum value
   maximum_value = array[0]
-  array.length.times do |index|
+  array.length-1.times do |index|
     if array[index] > maximum_value 
       maximum_value = array[index]
     end
